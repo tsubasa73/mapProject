51f86015d21151b874621b5a86900a36f006b751
matsuoka <matsuoka@gmail.com>
2021-02-13 04:02:49 +0900

    ・マップ画面の作成とAjax通信を実装

--- map/urls.py
+++ map/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import Index, MapWindow
+from .views import Index, MapWindow, AjaxResponse
 
 app_name = 'map'
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('window/', MapWindow.as_view(), name='window'),
+    path('window/ajax/', AjaxResponse.as_view(), name='ajax'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,14 +1,33 @@
-from django.views.generic import ListView
+import json
+from django.http import HttpResponse
+from django.views.generic import View, ListView
+from django.contrib.gis.geos import Polygon
+
 from .models import Location, Post
 
 
 class Index(ListView):
-    model = Location
-    queryset = Location.objects.all()
     template_name = 'map/index.html'
+    model = Location
 
 
 class MapWindow(ListView):
-    model = Location
-    queryset = Location.objects.all()
     template_name = 'map/window.html'
+    model = Location
+
+
+class AjaxResponse(View):
+    template_name = 'map/window.html'
+    model = Location
+
+    def get(self, request):
+        if request.is_ajax():
+            ymax = request.GET.get("map_ne_lat")
+            ymin = request.GET.get("map_sw_lat")
+            xmax = request.GET.get("map_ne_lng")
+            xmin = request.GET.get("map_sw_lng")
+            bbox = (xmin, ymin, xmax, ymax)
+            geom = Polygon.from_bbox(bbox)
+            queryset = Location.objects.filter(location__contained=geom)
+            points = [{'lat': obj.location.y, 'lng': obj.location.x} for obj in queryset]
+            return HttpResponse(json.dumps(points))
