00ced10c75b1394791ee7cec75a6493cb5d8d051
matsuoka <matsuoka@gmail.com>
2021-02-25 12:00:50 +0900

    本番環境に必要なライブラリを追加

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'map.apps.MapConfig',
     'leaflet',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -100,13 +101,27 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
-STATIC_ROOT = f'/var/www/{PROJECT_NAME}/static'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = f'/var/www/{PROJECT_NAME}/media'
-
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Static files settings
+STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# s3 static settings
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'mapProject.storage_backends.MediaStorage'
