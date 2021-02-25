94ef6b3d96f1ae9dd0340d366848524337eb6dc8
matsuoka <matsuoka@gmail.com>
2021-02-26 01:48:15 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'map.apps.MapConfig',
     'leaflet',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -107,21 +106,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files settings
 STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
 
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
+STATIC_URL = '/static/'
+STATIC_ROOT = f'/var/www/{PROJECT_NAME}/static'
 
-# s3 static settings
-AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'mapProject.storage_backends.MediaStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = f'/var/www/{PROJECT_NAME}/media'
